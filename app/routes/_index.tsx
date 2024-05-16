4cbf65475475e55d5327ca3dda5cc457dcc81228
stevenliao <steven.liaouey@gmail.com>
2024-05-16 14:09:22 -0400

    fix: fix bug loading index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,5 +3,5 @@ import type { LoaderFunctionArgs } from '@remix-run/node'
 export function loader({ response }: LoaderFunctionArgs) {
   response!.status = 302
   response!.headers.set('Location', '/products')
-  return null
+  return {}
 }
